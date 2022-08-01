a1473457d9c4caaa41334d25c30cbacde26f76d8
Stephanie Wilks <wilksstephanie.career@outlook.com>
2022-07-31 21:54:37 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const {bots, playerRecord} = require('./data')
-const {shuffleArray} = require('./utils')
+const express = require("express");
+const path = require("path");
+const app = express();
+const { bots, playerRecord } = require("./data");
+const { shuffleArray } = require("./utils");
 
+// include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
@@ -14,77 +15,90 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/index.html"));
-  });
+  res.sendFile(path.resolve("public/index.html"));
+});
 
-app.get('/api/robots', (req, res) => {
-    try {
-        res.status(200).send(bots)
-    } catch (error) {
-        console.log('ERROR GETTING BOTS', error)
-        res.sendStatus(400)
+app.get("/api/robots", (req, res) => {
+  try {
+    rollbar.info("Robots have loaded");
+    res.status(200).send(bots);
+  } catch (error) {
+    rollbar.error("Robots are not loaded");
+    console.log("ERROR GETTING BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/robots/five", (req, res) => {
+  try {
+    let shuffled = shuffleArray(bots);
+    let choices = shuffled.slice(0, 5);
+    let compDuo = shuffled.slice(6, 8);
+    rollbar.info(`Five robots have been loaded`)
+    res.status(200).send({ choices, compDuo });
+  } catch (error) {
+    rollbar.error("Unable to load five robots");
+    console.log("ERROR GETTING FIVE BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.post("/api/duel", (req, res) => {
+  try {
+    // getting the duos from the front end
+    let { compDuo, playerDuo } = req.body;
+
+    // adding up the computer player's total health and attack damage
+    let compHealth = compDuo[0].health + compDuo[1].health;
+    let compAttack =
+      compDuo[0].attacks[0].damage +
+      compDuo[0].attacks[1].damage +
+      compDuo[1].attacks[0].damage +
+      compDuo[1].attacks[1].damage;
+
+    // adding up the player's total health and attack damage
+    let playerHealth = playerDuo[0].health + playerDuo[1].health;
+    let playerAttack =
+      playerDuo[0].attacks[0].damage +
+      playerDuo[0].attacks[1].damage +
+      playerDuo[1].attacks[0].damage +
+      playerDuo[1].attacks[1].damage;
+
+    // calculating how much health is left after the attacks on each other
+    let compHealthAfterAttack = compHealth - playerAttack;
+    let playerHealthAfterAttack = playerHealth - compAttack;
+
+    // comparing the total health to determine a winner
+    if (compHealthAfterAttack > playerHealthAfterAttack) {
+      playerRecord.losses++;
+      res.status(200).send("You lost!");
+    } else {
+      playerRecord.wins++;
+      rollbar.info("Someone has one a duel");
+      res.status(200).send("You won!");
     }
-})
+  } catch (error) {
+    console.log("ERROR DUELING", error);
+    res.sendStatus(400);
+  }
+});
 
-app.get('/api/robots/five', (req, res) => {
-    try {
-        let shuffled = shuffleArray(bots)
-        let choices = shuffled.slice(0, 5)
-        let compDuo = shuffled.slice(6, 8)
-        res.status(200).send({choices, compDuo})
-    } catch (error) {
-        console.log('ERROR GETTING FIVE BOTS', error)
-        res.sendStatus(400)
-    }
-})
+app.get("/api/player", (req, res) => {
+  try {
+    res.status(200).send(playerRecord);
+  } catch (error) {
+    console.log("ERROR GETTING PLAYER STATS", error);
+    res.sendStatus(400);
+  }
+});
 
-app.post('/api/duel', (req, res) => {
-    try {
-        // getting the duos from the front end
-        let {compDuo, playerDuo} = req.body
-
-        // adding up the computer player's total health and attack damage
-        let compHealth = compDuo[0].health + compDuo[1].health
-        let compAttack = compDuo[0].attacks[0].damage + compDuo[0].attacks[1].damage + compDuo[1].attacks[0].damage + compDuo[1].attacks[1].damage
-        
-        // adding up the player's total health and attack damage
-        let playerHealth = playerDuo[0].health + playerDuo[1].health
-        let playerAttack = playerDuo[0].attacks[0].damage + playerDuo[0].attacks[1].damage + playerDuo[1].attacks[0].damage + playerDuo[1].attacks[1].damage
-        
-        // calculating how much health is left after the attacks on each other
-        let compHealthAfterAttack = compHealth - playerAttack
-        let playerHealthAfterAttack = playerHealth - compAttack
-
-        // comparing the total health to determine a winner
-        if (compHealthAfterAttack > playerHealthAfterAttack) {
-            playerRecord.losses++
-            res.status(200).send('You lost!')
-        } else {
-            playerRecord.wins++
-            res.status(200).send('You won!')
-        }
-    } catch (error) {
-        console.log('ERROR DUELING', error)
-        res.sendStatus(400)
-    }
-})
-
-app.get('/api/player', (req, res) => {
-    try {
-        res.status(200).send(playerRecord)
-    } catch (error) {
-        console.log('ERROR GETTING PLAYER STATS', error)
-        res.sendStatus(400)
-    }
-})
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
