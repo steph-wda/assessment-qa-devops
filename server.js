f25ab2d882185a24b0cd445e52c0bb1f2d1e9ef3
Stephanie Wilks <wilksstephanie.career@outlook.com>
2022-07-31 21:18:59 -0400

    rollbar

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express()
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "/public")));
